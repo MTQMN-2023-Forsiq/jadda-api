b6f8ea6e3dba0669cf5b6001097f306e157b8463
febiarifin <febiarifin0@gmail.com>
2023-09-14 11:02:16 +0700

    update API documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,13 @@
     <title>JADDA-API</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <body>
 
 <nav class="navbar bg-body-tertiary">
     <div class="container">
-        <span class="navbar-brand mb-0 h1">JADDA-API</span>
+        <span class="navbar-brand mb-0 h1"><i class="bi bi-book"></i> JADDA-API</span>
     </div>
 </nav>
 
@@ -32,16 +33,18 @@
         <div class="row g-2">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">Login <span class="badge bg-primary">POST</span></div>
                     <div class="card-body">
+                        <div class="border rounded p-1 mb-3">/login</div>
                         <img src="{{ asset('assets/api/login.png') }}" class="img-fluid">
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Register</div>
+                    <div class="card-header">Register <span class="badge bg-primary">POST</span></div>
                     <div class="card-body">
+                        <div class="border rounded p-1 mb-3">/register</div>
                         <img src="{{ asset('assets/api/login.png') }}" class="img-fluid">
                     </div>
                 </div>
@@ -54,8 +57,9 @@
         <div class="row g-2">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Get User Detail</div>
+                    <div class="card-header">Get User Detail <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
+                        <div class="border rounded p-1 mb-3">/user</div>
                         <img src="{{ asset('assets/api/user-detail.png') }}" class="img-fluid">
                     </div>
                 </div>
@@ -68,16 +72,18 @@
         <div class="row g-2">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Add Point User</div>
+                    <div class="card-header">Add Point User <span class="badge bg-primary">POST</span></div>
                     <div class="card-body">
+                        <div class="border rounded p-1 mb-3">/point</div>
                         <img src="{{ asset('assets/api/add-point.png') }}" class="img-fluid">
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Get Leaderboard</div>
+                    <div class="card-header">Get Leaderboard <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
+                        <div class="border rounded p-1 mb-3">/leaderboard</div>
                         <img src="{{ asset('assets/api/leaderboard.png') }}" class="img-fluid">
                     </div>
                 </div>
@@ -90,20 +96,20 @@
         <div class="row g-2">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Get All Tajweeds</div>
+                    <div class="card-header">Get All Tajweeds <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/tajweeds</div>
-                        <a href="{{ route('get.all.tajweed') }}" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="{{ route('get.all.tajweed') }}" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Get Tajweed by ID</div>
+                    <div class="card-header">Get Tajweed by ID <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/tajweed/{id}</div>
-                        <a href="{{ route('get.tajweed.by.id', 1) }}" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="{{ route('get.tajweed.by.id', 1) }}" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -116,10 +122,10 @@
         <div class="row g-2">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Get All Tafsir</div>
+                    <div class="card-header">Get All Tafsir <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/tafsirs</div>
-                        <a href="{{ route('get.all.tafsir') }}" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="{{ route('get.all.tafsir') }}" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -144,7 +150,7 @@
                     <div class="card-header">Get List Surah</div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/surah</div>
-                        <a href="https://api.quran.gading.dev/surah" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="https://api.quran.gading.dev/surah" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -154,7 +160,7 @@
                     <div class="card-header">Get Surah by Id</div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/surah/{surah_id}</div>
-                        <a href="https://api.quran.gading.dev/surah/1" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="https://api.quran.gading.dev/surah/1" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -164,7 +170,7 @@
                     <div class="card-header">Get Juz By Id</div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/juz/{juz_id}</div>
-                        <a href="https://api.quran.gading.dev/juz/1" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="https://api.quran.gading.dev/juz/1" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -185,10 +191,10 @@
             </div>
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Get List Hadist Abu Daud (20)</div>
+                    <div class="card-header">Get List Hadist Abu Daud (20) <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/books/abu-daud?range=1-20</div>
-                        <a href="https://api.hadith.gading.dev/books/abu-daud?range=1-20" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="https://api.hadith.gading.dev/books/abu-daud?range=1-20" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
@@ -209,10 +215,10 @@
             </div>
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Get Jadwal Sholat by Lokasi</div>
+                    <div class="card-header">Get Jadwal Sholat by Lokasi <span class="badge bg-primary">GET</span></div>
                     <div class="card-body">
                         <div class="border rounded p-1 mb-3">/timingsByCity/[DATE]?city=[CITY_NAME]&country=Indonesia&method=8</div>
-                        <a href="https://api.aladhan.com/v1/timingsByCity/24-08-2023?city=Wonosobo&country=Indonesia&method=8" target="_blank" class="btn btn-primary btn-sm">Run
+                        <a href="https://api.aladhan.com/v1/timingsByCity/24-08-2023?city=Wonosobo&country=Indonesia&method=8" target="_blank" class="btn btn-success btn-sm"><i class="bi bi-play-circle"></i> Run
                             Example</a>
                     </div>
                 </div>
