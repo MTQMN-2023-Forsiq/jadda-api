25b89472168f14b34ed8aa2da9f3198cea591b2d
febiarifin <febiarifin0@gmail.com>
2023-09-25 15:59:53 +0700

    update API kalender hijriah

--- app/Http/Controllers/API/JadwalSholatController.php
+++ app/Http/Controllers/API/JadwalSholatController.php
@@ -16,10 +16,12 @@ class JadwalSholatController extends Controller
     {
         $shedule_sholat = Http::get('https://api.aladhan.com/v1/timingsByCity/'.now()->format('d-m-Y').'?city='.$city.'&country=Indonesia&method=8')['data']['timings'];
         $national_date = now()->dayName.', '.now()->day.' '.now()->monthName.' '.now()->year;
-        $hijriah_date = Http::get('http://api.aladhan.com/v1/gToH/'.now()->format('d-m-Y'))['data']['hijri'];
+        //$hijriah_date = Http::get('http://api.aladhan.com/v1/gToH/'.now()->format('d-m-Y'))['data']['hijri'];
+        $hijriah_date = Http::get('https://service.unisayogya.ac.id/kalender/api/masehi2hijriah/muhammadiyah/'.now()->format('Y/m/d'));
         return $this->success([
             "national_date" => $national_date,
-            "hijriah_date" => $hijriah_date['day'].' '.$hijriah_date['month']['en'].' '.$hijriah_date['year'].'H',
+            //"hijriah_date" => $hijriah_date['day'].' '.$hijriah_date['month']['en'].' '.$hijriah_date['year'].'H',
+            "hijriah_date" => $hijriah_date['tanggal'].' '.$hijriah_date['namabulan'].' '.$hijriah_date['tahun'].'H',
             "times" => $shedule_sholat,
         ]);
     }
