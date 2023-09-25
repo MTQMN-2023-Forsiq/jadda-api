e23d5932259c1bfc7098c0f14cdfd9d912036b61
febiarifin <febiarifin0@gmail.com>
2023-09-25 15:53:07 +0700

    fix bug on API kalender hijriah

--- app/Http/Controllers/API/JadwalSholatController.php
+++ app/Http/Controllers/API/JadwalSholatController.php
@@ -16,7 +16,7 @@ class JadwalSholatController extends Controller
     {
         $shedule_sholat = Http::get('https://api.aladhan.com/v1/timingsByCity/'.now()->format('d-m-Y').'?city='.$city.'&country=Indonesia&method=8')['data']['timings'];
         $national_date = now()->dayName.', '.now()->day.' '.now()->monthName.' '.now()->year;
-        $hijriah_date = Http::get('http://api.aladhan.com/v1/gToH/07-12-2014')['data']['hijri'];
+        $hijriah_date = Http::get('http://api.aladhan.com/v1/gToH/'.now()->format('d-m-Y'))['data']['hijri'];
         return $this->success([
             "national_date" => $national_date,
             "hijriah_date" => $hijriah_date['day'].' '.$hijriah_date['month']['en'].' '.$hijriah_date['year'].'H',
